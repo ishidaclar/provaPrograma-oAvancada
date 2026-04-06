7caf76cf7afb00bd60b55a4155aacc0f50d7824c
clara <cishidadias@gmail.com>
2026-04-06 19:18:47 -0300

    repository criado

---
+++ src/test/java/com/clara/claraprovaprogramacaoavancada/repositorys/ProdutoRepository.java
@@ -0,0 +1,6 @@
+package com.clara.claraprovaprogramacaoavancada.repositorys;
+import com.clara.claraprovaprogramacaoavancada.models.ProdutoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+}
